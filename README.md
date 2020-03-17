b048c535e9bd2c2cc7436d63f2245649d2923777
Yifei Yu <Chris@Encyclopedia-Galactica.local>
2020-03-16 22:41:31 -0500

    Typo correction

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Equity Return Correlation Analysis
-The final project for E177 SPR20 Time Series Analysis analyses several time series vectors that are potentially correlated with Tesla's Equity Return  
+The final project for E177 SPR20 Time Series Analysis analyses several time series vectors that are potentially correlated with Honda Motor's Equity Return  
 This project is motivated by the large variance of equity return
--- analyse.py
+++ analyse.py
@@ -1,3 +1,23 @@
 '''
-This script analyses imported data with linear model, cointergration and regime switching
+This script analyses imported data with linear model, cointergration, AR + GARCH (1, 1) and regime switching
 '''
+
+# Import libraries
+import statsmodels.tsa.api as smt
+import statsmodels.api as sm
+from arch import arch_model
+
+# Define global variables
+
+class Analysis:
+	def linear_model():
+		print('Okay')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	print('Okay')
